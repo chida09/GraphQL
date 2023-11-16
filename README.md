878d6855a8c47dbe516156fd48c46f40b3c4c7e6
takaaki chida <takaaki.chida@findy.co.jp>
2023-11-17 08:43:23 +0900

    add doc

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # GraphQL
-https://reffect.co.jp/html/graphql/
+
+## 参考情報
+* [これを読めばGraphQL全体がわかる。GraphQLサーバからDB、フロントエンド構築](https://reffect.co.jp/html/graphql/)
+
+## 基本動作確認
+1. index.jsにコードをコピペ
+2. $ npx index.jsでサーバー起動
+3. consoleに表示されたurlにアクセス
+4. Apollo Studio Explorerが起動するので、Query your serverをクリック
+5. Operations を空にした状態から左側の Documentationの Query と Fields の下にある hello をクリック
+
+```
+operation:
+query Query {
+    hello
+}
+
+output:
+{
+    "data": {
+     "hello": "Hello World"
+    }
+}
+```
--- index-1.js
+++ index-1.js
@@ -1,24 +1,3 @@
-/**
- * 基本動作確認
- * 1. index.jsにコードをコピペ
- * 2. $ npx index.jsでサーバー起動
- * 3. consoleに表示されたurlにアクセス
- * 4. Apollo Studio Explorerが起動するので、Query your serverをクリック
- * 5. Operations を空にした状態から左側の Documentationの Query と Fields の下にある hello をクリック
- *
- * operation:
- * query Query {
- *   hello
- * }
- *
- * output:
- * {
- *   "data": {
- *     "hello": "Hello World"
- *   }
- * }
- */
-
 const { ApolloServer, gql } = require('apollo-server')
 
 // Queryタイプ: Read Operation(問い合わせ)を行うクエリーの定義に利用します
